a7780faa0624197bedf3d5f65b45f2cbb338497b
Ralf Guder <ralfguder@t-online.de>
2021-09-24 13:40:50 +0200

    add 19

--- src/main/java/gudchensoft/kuno/Kuno.java
+++ src/main/java/gudchensoft/kuno/Kuno.java
@@ -5,14 +5,11 @@ import java.io.InputStream;
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
-import org.newdawn.slick.ImageBuffer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.SpriteSheet;
-import org.newdawn.slick.opengl.ImageDataFactory;
 
 /**
  * 
