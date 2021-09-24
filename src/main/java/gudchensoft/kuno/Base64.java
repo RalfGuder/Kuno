6f9bfee7596cad9b488c48fd4216d1dac89e5dbc
Ralf Guder <ralfguder@t-online.de>
2021-09-24 10:59:24 +0200

    add 10

--- src/main/java/gudchensoft/kuno/Base64.java
+++ src/main/java/gudchensoft/kuno/Base64.java
@@ -1,38 +1,30 @@
 package gudchensoft.kuno;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.util.Base64.Encoder;
 
 public class Base64 {
 	public static void main(String[] args) {
+		Base64 base = new Base64();
 
 		OutputStream os = null;
 		InputStream is = null;
+
+
+		
 		try {
 
 
-			is = Base64.class.getResourceAsStream("kuno-sprites.png");
+			is = Base64.class.getResourceAsStream("/kuno-sprites.png");
 
 			String dateipfad = "C:/Anhang/kuno-sprites.b64";
 			File file = new File(dateipfad);
 			os = new FileOutputStream(file);
-
-			Encoder ec = java.util.Base64.getEncoder();
-			os = ec.wrap(os);
-
-			byte[] buffer = new byte[1024];
-			int anzahl;
-			while ((anzahl = is.read(buffer, 0, buffer.length)) != -1) {
-
-				os.write(buffer, 0, anzahl);
-			}
+			base.write(is, os);
 			os.close();
 			is.close();
 
@@ -45,6 +37,19 @@ public class Base64 {
 
 		}
 
+	}
+
+	private void write(InputStream is, OutputStream os) throws IOException {
+		os = java.util.Base64.getEncoder().wrap(os);
+
+		byte[] buffer = new byte[1024];
+		int anzahl;
+		while ((anzahl = is.read(buffer, 0, buffer.length)) != -1) {
+
+			os.write(buffer, 0, anzahl);
+		}
+
+		
 	}
 
 }
